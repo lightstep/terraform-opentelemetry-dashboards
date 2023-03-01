34a19c7726bfd9a1abef79f0cfc671775117ac08
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-01 16:08:40 -0600

    Switch Cilium dashboard version to 1.70.10

--- collector-dashboards/otel-collector-cilium-overview-dashboard/main.tf
+++ collector-dashboards/otel-collector-cilium-overview-dashboard/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = "~> 1.70.11"
+      version = "~> 1.70.10"
     }
   }
   required_version = ">= v1.0.11"
