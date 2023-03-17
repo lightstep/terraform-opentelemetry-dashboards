dfe04ff107addff5dd0beeb201fdc4849432a35a
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-17 16:03:07 -0500

    fix output var resource types

--- collector-dashboards/otel-collector-k8s-kubelet-prom-dashboard/outputs.tf
+++ collector-dashboards/otel-collector-k8s-kubelet-prom-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_metric_dashboard.k8s_kubelet_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.k8s_kubelet_dashboard.id}"
   description = "k8s resources"
 }
--- collector-dashboards/otel-collector-k8s-pod-resources-prom-dashboard/outputs.tf
+++ collector-dashboards/otel-collector-k8s-pod-resources-prom-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_metric_dashboard.k8s_resources_pod_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.k8s_resources_pod_dashboard.id}"
   description = "k8s resources"
 }
