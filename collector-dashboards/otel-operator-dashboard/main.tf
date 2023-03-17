24e393a658a5589d133950a59e6c9502941d1b75
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-17 14:41:44 -0500

    update otel-operator syntax

--- collector-dashboards/otel-operator-dashboard/main.tf
+++ collector-dashboards/otel-operator-dashboard/main.tf
@@ -2,212 +2,147 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = "~> 1.70.6"
+      version = "~> 1.70.10"
     }
   }
   required_version = ">= v1.0.11"
 }
 
-resource "lightstep_metric_dashboard" "otel_operator_dashboard" {
-    dashboard_name = "OpenTelemetry Operator"
-    project_name   = var.lightstep_project
+resource "lightstep_dashboard" "otel_operator_dashboard_dashboard" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "OpenTelemetry Operator"
+  dashboard_description = ""
 
-    chart {
-        name = "Restarts in the past 30 mins"
-        rank = 5
-        type = "timeseries"
+  chart {
+    name = "Total reconciles"
+    rank = "0"
+    type = "timeseries"
 
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "kube_pod_container_status_restarts_total"
-            query_name          = "a"
-            timeseries_operator = "rate"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "pod",
-                ]
-            }
-        }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric controller_runtime_reconcile_total | delta | group_by ["controller", "result", "pod"], sum
+EOT
     }
-    chart {
-        name = "Reconcile Errors"
-        rank = 2
-        type = "timeseries"
 
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "controller_runtime_reconcile_errors_total"
-            query_name          = "a"
-            timeseries_operator = "delta"
+  }
 
-            group_by {
-                aggregation_method = "max"
-                keys               = [
-                    "controller",
-                ]
-            }
-        }
+  chart {
+    name = "Time to reconcile"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "heatmap"
+      hidden       = false
+      query_string = <<EOT
+metric controller_runtime_reconcile_time_seconds | delta | group_by ["controller"], sum
+EOT
     }
-    chart {
-        name = "Time to reconcile"
-        rank = 1
-        type = "timeseries"
 
-        query {
-            display             = "heatmap"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "controller_runtime_reconcile_time_seconds"
-            query_name          = "a"
-            timeseries_operator = "delta"
+  }
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "controller",
-                ]
-            }
-        }
+  chart {
+    name = "Reconcile Errors"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = <<EOT
+metric controller_runtime_reconcile_errors_total | delta | group_by ["controller"], max
+EOT
     }
-    chart {
-        name = "Total reconciles"
-        rank = 0
-        type = "timeseries"
 
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "controller_runtime_reconcile_total"
-            query_name          = "a"
-            timeseries_operator = "delta"
+  }
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "controller",
-                    "result",
-                    "pod",
-                ]
-            }
-        }
+  chart {
+    name = "CPU Usage"
+    rank = "3"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric container_cpu_usage_seconds_total | rate | group_by ["pod"], sum
+EOT
     }
-    chart {
-        name = "CPU Usage"
-        rank = 3
-        type = "timeseries"
 
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "container_cpu_usage_seconds_total"
-            query_name          = "a"
-            timeseries_operator = "rate"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "pod",
-                ]
-            }
-        }
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "kube_pod_container_resource_requests"
-            query_name          = "b"
-            timeseries_operator = "last"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "pod",
-                ]
-            }
-        }
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "kube_pod_container_resource_limits"
-            query_name          = "c"
-            timeseries_operator = "last"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "pod",
-                ]
-            }
-        }
+    query {
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kube_pod_container_resource_requests | latest | group_by ["pod"], sum
+EOT
     }
-    chart {
-        name = "Memory Usage"
-        rank = 4
-        type = "timeseries"
 
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "container_memory_working_set_bytes"
-            query_name          = "a"
-            timeseries_operator = "last"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "pod",
-                ]
-            }
-        }
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "kube_pod_container_resource_requests"
-            query_name          = "b"
-            timeseries_operator = "last"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "pod",
-                ]
-            }
-        }
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "kube_pod_container_resource_limits"
-            query_name          = "c"
-            timeseries_operator = "last"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "pod",
-                ]
-            }
-        }
+    query {
+      query_name   = "c"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kube_pod_container_resource_limits | latest | group_by ["pod"], sum
+EOT
     }
+
+  }
+
+  chart {
+    name = "Memory Usage"
+    rank = "4"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric container_memory_working_set_bytes | latest | group_by ["pod"], sum
+EOT
+    }
+
+    query {
+      query_name   = "b"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kube_pod_container_resource_requests | latest | group_by ["pod"], sum
+EOT
+    }
+
+    query {
+      query_name   = "c"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kube_pod_container_resource_limits | latest | group_by ["pod"], sum
+EOT
+    }
+
+  }
+
+  chart {
+    name = "Restarts in the past 30 mins"
+    rank = "5"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = <<EOT
+metric kube_pod_container_status_restarts_total | rate | group_by ["pod"], sum
+EOT
+    }
+
+  }
+
 }
