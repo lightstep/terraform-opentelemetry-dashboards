fd536316148238fc9919d23a1bd392f7952948f7
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-20 17:14:40 -0500

    update otel-target-allocator

--- collector-dashboards/otel-target-allocator-dashboard/outputs.tf
+++ collector-dashboards/otel-target-allocator-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_metric_dashboard.otel_target_allocator_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_target_allocator_dashboard.id}"
   description = "OpenTelemetry Collector Dashboard URL"
 }
