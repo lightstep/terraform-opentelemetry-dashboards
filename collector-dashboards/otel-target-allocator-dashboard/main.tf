e2effdf8ae31cf4b33344ceef883a1732c2a5989
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-20 17:19:07 -0500

    update otel-target-allocator (#44)

--- collector-dashboards/otel-target-allocator-dashboard/main.tf
+++ collector-dashboards/otel-target-allocator-dashboard/main.tf
@@ -2,215 +2,176 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = "~> 1.70.6"
+      version = "~> 1.70.10"
     }
   }
   required_version = ">= v1.0.11"
 }
-resource "lightstep_metric_dashboard" "otel_target_allocator_dashboard" {
-    dashboard_name = "OpenTelemetry Target Allocator"
-    project_name   = var.lightstep_project
 
-    chart {
-        name = "Allocatable vs discovered"
-        rank = 1
-        type = "timeseries"
+resource "lightstep_dashboard" "otel_target_allocator_dashboard" {
+  project_name          = var.lightstep_project
+  dashboard_name        = "OpenTelemetry Target Allocator"
+  dashboard_description = "Monitor OpenTelemetry Target Allocator with this overview dashboard."
 
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "opentelemetry_allocator_collectors_allocatable"
-            query_name          = "a"
-            timeseries_operator = "last"
+  chart {
+    name = "Targets per Collector"
+    rank = "0"
+    type = "timeseries"
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = []
-            }
-        }
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "opentelemetry_allocator_collectors_discovered"
-            query_name          = "b"
-            timeseries_operator = "last"
-
-            group_by {
-                aggregation_method = "sum"
-                keys               = []
-            }
-        }
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = <<EOT
+metric opentelemetry_allocator_targets_per_collector | latest | group_by ["collector_name"], sum
+EOT
     }
-    chart {
-        name = "Targets per collector"
-        rank = 0
-        type = "timeseries"
 
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "opentelemetry_allocator_targets_per_collector"
-            query_name          = "a"
-            timeseries_operator = "last"
+  }
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "collector_name",
-                ]
-            }
-        }
+  chart {
+    name = "Allocatable vs. Discovered"
+    rank = "1"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric opentelemetry_allocator_collectors_allocatable | latest | group_by [], sum"
     }
-    chart {
-        name = "Time to Allocate"
-        rank = 6
-        type = "timeseries"
 
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            query_name          = "a"
-            tql                 = <<-EOT
-                metric opentelemetry_allocator_time_to_allocate
-                | delta 5m
-                | group_by [method], sum
-                | point percentile(value, 50.0), percentile(value, 95.0), percentile(value, 99.0)
-            EOT
-        }
+    query {
+      query_name   = "b"
+      display      = "bar"
+      hidden       = false
+      query_string = "metric opentelemetry_allocator_collectors_discovered | latest | group_by [], sum"
     }
-    chart {
-        name = "Targets Discovered"
-        rank = 3
-        type = "timeseries"
 
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "opentelemetry_allocator_targets"
-            query_name          = "a"
-            timeseries_operator = "last"
+  }
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "job_name",
-                ]
-            }
-        }
+  chart {
+    name = "Allocator Events"
+    rank = "2"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = <<EOT
+metric opentelemetry_allocator_events | delta | group_by ["source"], sum
+EOT
     }
-    chart {
-        name = "Target allocator scrapes"
-        rank = 7
-        type = "timeseries"
 
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "scrape_series_added"
-            query_name          = "a"
-            timeseries_operator = "last"
+  }
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "collector.name",
-                ]
-            }
-        }
+  chart {
+    name = "Targets Discovered"
+    rank = "3"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = <<EOT
+metric opentelemetry_allocator_targets | latest | group_by ["job_name"], sum
+EOT
     }
-    chart {
-        name = "HTTP Response Time"
-        rank = 4
-        type = "timeseries"
 
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            query_name          = "a"
-            tql                 = <<-EOT
-                metric opentelemetry_allocator_http_duration_seconds
-                | delta 5m
-                | group_by [path], sum
-                | point percentile(value, 50.0), percentile(value, 95.0), percentile(value, 99.0)
-            EOT
-        }
+  }
+
+  chart {
+    name = "HTTP Response Time"
+    rank = "4"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric opentelemetry_allocator_http_duration_seconds
+| delta 5m
+| group_by [path], sum
+| point percentile(value, 50.0), percentile(value, 95.0), percentile(value, 99.0)
+
+EOT
     }
-    chart {
-        name = "Kubernetes events"
-        rank = 5
-        type = "timeseries"
 
-        query {
-            display             = "line"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "prometheus_sd_kubernetes_events_total"
-            query_name          = "a"
-            timeseries_operator = "rate"
+  }
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "event",
-                    "role",
-                ]
-            }
-        }
+  chart {
+    name = "Kubernetes Events"
+    rank = "5"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric prometheus_sd_kubernetes_events_total | rate | group_by ["event", "role"], sum
+EOT
     }
-    chart {
-        name = "Allocation by method count"
-        rank = 8
-        type = "timeseries"
 
-        query {
-            display         = "bar"
-            exclude_filters = []
-            hidden          = false
-            include_filters = []
-            query_name      = "a"
-            tql             = <<-EOT
-                metric opentelemetry_allocator_time_to_allocate
-                | delta 5m
-                | group_by [method], sum
-                | point dist_count(value)
-            EOT
-        }
+  }
+
+  chart {
+    name = "Time to Allocate"
+    rank = "6"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric opentelemetry_allocator_time_to_allocate
+| delta 5m
+| group_by [method], sum
+| point percentile(value, 50.0), percentile(value, 95.0), percentile(value, 99.0)
+
+EOT
     }
-    chart {
-        name = "Allocator Events"
-        rank = 2
-        type = "timeseries"
 
-        query {
-            display             = "bar"
-            exclude_filters     = []
-            hidden              = false
-            include_filters     = []
-            metric              = "opentelemetry_allocator_events"
-            query_name          = "a"
-            timeseries_operator = "delta"
+  }
 
-            group_by {
-                aggregation_method = "sum"
-                keys               = [
-                    "source",
-                ]
-            }
-        }
+  chart {
+    name = "Target Allocator Scrapes"
+    rank = "7"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = <<EOT
+metric scrape_series_added | latest | group_by ["collector.name"], sum
+EOT
     }
+
+  }
+
+  chart {
+    name = "Allocation by Method Count"
+    rank = "8"
+    type = "timeseries"
+
+    query {
+      query_name   = "a"
+      display      = "bar"
+      hidden       = false
+      query_string = <<EOT
+metric opentelemetry_allocator_time_to_allocate
+| delta 5m
+| group_by [method], sum
+| point dist_count(value)
+
+EOT
+    }
+
+  }
+
 }
