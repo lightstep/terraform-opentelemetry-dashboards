4418f29625d915b9022c6c1defc6eb5e62b6d92c
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-03-28 10:46:38 -0500

    Add hostmetrics Disk dashboard

--- collector-dashboards/otel-collector-hostmetrics-disk-dashboard/outputs.tf
+++ collector-dashboards/otel-collector-hostmetrics-disk-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_metric_dashboard.otel_collector_hostmetrics_disk_dashboard.id}"
-  description = "OpenTelemetry Collector Host Metrics Disk Dashboard URL"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.oetl_collector_hostmetrics_disk.id}"
+  description = "Hostmetrics Disk Dashboard URL"
 }
