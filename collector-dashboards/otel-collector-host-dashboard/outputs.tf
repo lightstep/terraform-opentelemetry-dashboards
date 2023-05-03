fd3ea69cea0d28561f7bdffd1531d877eb728567
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-05-03 14:38:10 -0500

    Update host dashboard to use only UQL (#77)

--- collector-dashboards/otel-collector-host-dashboard/outputs.tf
+++ collector-dashboards/otel-collector-host-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_metric_dashboard.otel_collector_host_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_host_dashboard.id}"
   description = "OpenTelemetry Collector Host Dashboard URL"
 }
