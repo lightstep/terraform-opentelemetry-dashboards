359dbad3603e1e5b9e3cc823f9c3bfadec4d8d75
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-05-02 22:52:33 -0500

    Update Nginx receiver

--- collector-dashboards/otel-collector-nginxreceiver-dashboard/outputs.tf
+++ collector-dashboards/otel-collector-nginxreceiver-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_metric_dashboard.otel_collector_nginxreceiver_dashboard.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_nginxreceiver_dashboard.id}"
   description = "OpenTelemetry Nginx Dashboard URL"
 }
