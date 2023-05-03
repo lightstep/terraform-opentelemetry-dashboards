8caeaff4370af2fc5549082e8b67ab26d4eb423f
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-05-03 14:35:31 -0500

    Updates Kafka dashboard to UQL (#75)

--- collector-dashboards/otel-collector-kafka-dashboard/main.tf
+++ collector-dashboards/otel-collector-kafka-dashboard/main.tf
@@ -8,250 +8,173 @@ terraform {
   required_version = ">= v1.0.11"
 }
 
-resource "lightstep_metric_dashboard" "otel_collector_kafka_dashboard" {
-    project_name   = var.lightstep_project
-    dashboard_name = "OpenTelemetry Collector - Kafka Metrics"
+resource "lightstep_dashboard" "otel_collector_kafka_dashboard" {
+  project_name   = var.lightstep_project
+  dashboard_name = "OpenTelemetry Collector - Kafka Metrics"
 
-    chart {
-      name = "Broker count"
-      rank = "0"
-      type = "timeseries"
+  chart {
+    name = "Broker Count"
+    rank = "0"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.brokers"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = []
-        }
-
-        # TODO: Unit "{brokers}"
-        # TODO: Number of brokers in the cluster.
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.brokers | last | group_by [], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka partitions per topic"
-      rank = "1"
-      type = "timeseries"
+  chart {
+    name = "Kafka Partitions Per Topic"
+    rank = "1"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.topic.partitions"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["topic"]
-        }
-        # TODO: add description: Number of partitions in topic.
-        # TODO: add unit: "{partitions}"
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.topic.partitions | last | group_by ["topic"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka partitions current offset"
-      rank = "2"
-      type = "timeseries"
+  chart {
+    name = "Kafka Partitions Current Offset"
+    rank = "2"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.partition.current_offset"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["topic", "partition"]
-        }
-        # TODO: add description: Current offset of partition of topic.
-        # TODO: add unit: 1
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.partition.current_offset | last | group_by ["topic", "partition"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka partitions oldest offset"
-      rank = "3"
-      type = "timeseries"
+  chart {
+    name = "Kafka Partitions Oldest Offset"
+    rank = "3"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.partition.oldest_offset"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["topic", "partition"]
-        }
-        # TODO: add description: Oldest offset of partition of topic
-        # TODO: add unit: 1
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.partition.oldest_offset | last | group_by ["topic", "partition"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka partition replicas"
-      rank = "4"
-      type = "timeseries"
+  chart {
+    name = "Kafka Partition Replicas"
+    rank = "4"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.partition.replicas"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["topic", "partition"]
-        }
-        # TODO: add description: Number of replicas for partition of topic
-        # TODO: add unit: "{replicas}"
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.partition.replicas | last | group_by ["topic", "partition"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka synchronized partition replicas"
-      rank = "5"
-      type = "timeseries"
+  chart {
+    name = "Kafka Synchronized Partition Replicas"
+    rank = "5"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.partition.replicas_in_sync"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["topic", "partition"]
-        }
-        # TODO: add description: Number of synchronized replicas of partition
-        # TODO: add unit: "{replicas}"
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.partition.replicas_in_sync | last | group_by ["topic", "partition"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka consumer group members"
-      rank = "6"
-      type = "timeseries"
+  chart {
+    name = "Kafka Consumer Group Members"
+    rank = "6"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.consumer_group.members"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["group"]
-        }
-        # TODO: add description: Count of members in the consumer group
-        # TODO: add unit: "{members}"
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.consumer_group.members | last | group_by ["group"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka consumer group offset"
-      rank = "7"
-      type = "timeseries"
+  chart {
+    name = "Kafka Consumer Group Offset"
+    rank = "7"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.consumer_group.offset"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["group", "topic", "partition"]
-        }
-        # TODO: add description: Current offset of the consumer group at partition of topic
-        # TODO: add unit: 1
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.consumer_group.offset | last | group_by ["group", "topic", "partition"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka consumer group offset sum"
-      rank = "8"
-      type = "timeseries"
+  chart {
+    name = "Kafka Consumer Group Offset Sum"
+    rank = "8"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.consumer_group.offset_sum"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["group", "topic"]
-        }
-        # TODO: add description: Sum of consumer group offset across partitions of topic
-        # TODO: add unit: 1
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.consumer_group.offset_sum | last | group_by ["group", "topic"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka consumer group lag"
-      rank = "9"
-      type = "timeseries"
+  chart {
+    name = "Kafka Consumer Group Lag"
+    rank = "9"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.consumer_group.lag"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["group", "topic", "partition"]
-        }
-        # TODO: add description: Current approximate lag of consumer group at partition of topic
-        # TODO: add unit: 1
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.consumer_group.lag | last | group_by ["group", "topic", "partition"], sum
+EOT
     }
+  }
 
-    chart {
-      name = "Kafka consumer group lag sum"
-      rank = "10"
-      type = "timeseries"
+  chart {
+    name = "Kafka Consumer Group Lag Sum"
+    rank = "10"
+    type = "timeseries"
 
-      query {
-        query_name = "a"
-        display    = "line"
-        hidden     = false
-
-        metric              = "kafka.consumer_group.lag_sum"
-        timeseries_operator = "last"
-
-        group_by {
-          aggregation_method = "sum"
-          keys               = ["group", "topic"]
-        }
-        # TODO: add description: Current approximate sum of consumer group lag across all partitions of topic
-        # TODO: add unit: 1
-      }
+    query {
+      query_name   = "a"
+      display      = "line"
+      hidden       = false
+      query_string = <<EOT
+metric kafka.consumer_group.lag_sum | last | group_by ["group", "topic"], sum
+EOT
     }
+  }
 }
+
