91d7d184abfe7fe4bd532f2d5458f03c9162b50b
Heber Silva <heber.silva@lightstep.com>
2023-06-29 13:35:50 -0400

    bumped version

--- collector-dashboards/otel-collector-ibmmq-dashboard/main.tf
+++ collector-dashboards/otel-collector-ibmmq-dashboard/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     lightstep = {
       source  = "lightstep/lightstep"
-      version = "~> 1.70.10"
+      version = "~> 1.76.10"
     }
   }
   required_version = ">= v1.0.11"
