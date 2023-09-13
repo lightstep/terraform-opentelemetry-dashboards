0b78de93e50b37a24acc475302925947d08f7057
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-09-12 21:57:18 -0500

    rebrand corrections

--- collector-dashboards/otel-collector-jbosswildfly-prom-receiver-dashboard/main.tf
+++ collector-dashboards/otel-collector-jbosswildfly-prom-receiver-dashboard/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 resource "lightstep_dashboard" "otel_collector_wildfly_dashboard" {
-  project_name          = var.lightstep_project
+  project_name          = var.cloud_observability_project
   dashboard_name        = "OpenTelemetry JBoss Wildfly Dashboard"
   dashboard_description = "Monitor JBoss Wildfly metrics with this summary dashboard."
 
