d8e22ef325bc60c62b6f415086ad8dbf1330bddd
Nathan Slaughter <28688390+nslaughter@users.noreply.github.com>
2023-09-20 14:43:43 -0500

    bump errant version strings

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
-variable "cloud_observability_project" {
+variable "lightstep_project" {
   description = "Name of Cloud Observability project"
   type        = string
 }
 
-variable "cloud_observability_organization" {
+variable "lightstep_organization" {
   description = "Name of Cloud Observability organization"
   type        = string
 }
 
-variable "cloud_observability_env" {
+variable "lightstep_env" {
   description = "Cloud Observability environment"
   type        = string
   default     = "public"
 }
 
-variable "cloud_observability_api_key_env_var" {
+variable "lightstep_api_key_env_var" {
   description = "Name of the local environment variable that contains the Cloud Observability API key"
   type        = string
   default     = "LIGHTSTEP_API_KEY"
