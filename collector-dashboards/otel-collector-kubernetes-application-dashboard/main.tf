3c48b28f37b9e95a46b292d6d93e5cf3281e8fac
Tani Pham <tania.pham@lightstep.com>
2023-09-20 14:55:43 -0700

    fixed main.tf project_name

--- collector-dashboards/otel-collector-kubernetes-application-dashboard/main.tf
+++ collector-dashboards/otel-collector-kubernetes-application-dashboard/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 resource "lightstep_dashboard" "otel_collector_kubernetes_application_dashboard" {
   dashboard_name        = "Kubernetes Application"
-  project_name          = "OpenTelemetry Kubernetes Application"
+  project_name          = var.cloud_observability_project
   dashboard_description = "This dashboard is a generic kubernetes application and machine dashboard, which uses OTEL receivers (k8scluster, hostmetrics, kubeletstats) to generate infrastructure metrics."
 
   group {
