f5ac55670f6175817e8b4b53478dbc7d56bbb2c0
Tani Pham <tania.pham@lightstep.com>
2023-09-21 10:59:35 -0700

    updated outputs.tf

--- collector-dashboards/otel-collector-kubernetes-application-dashboard/outputs.tf
+++ collector-dashboards/otel-collector-kubernetes-application-dashboard/outputs.tf
@@ -1,4 +1,4 @@
 output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.cloud_observability_project}/dashboard/${lightstep_dashboard.otel_collector_kubernetes_application.id}"
+  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_kubernetes_application.id}"
   description = "OpenTelemetry Collector Kubernetes Application Dashboard URL"
 }
