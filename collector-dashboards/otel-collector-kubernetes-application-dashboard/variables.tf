ce1d084710c169837d1fd1efb824eea2d843a7a3
Tani Pham <tania.pham@lightstep.com>
2023-09-21 10:47:39 -0700

    updated variables.tf

--- collector-dashboards/otel-collector-kubernetes-application-dashboard/variables.tf
+++ collector-dashboards/otel-collector-kubernetes-application-dashboard/variables.tf
@@ -1,4 +1,4 @@
-variable "cloud_observability_project" {
-  description = "Name of Cloud Observability project"
+variable "lightstep_project" {
+  description = "Name of Lightstep project"
   type        = string
 }
