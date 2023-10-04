b4a796bc5c3e2b1123d02ec3f3cabd65bcfa6909
Dmitry Av <altuner@users.noreply.github.com>
2023-10-04 14:49:07 -0700

    Deleted empty couchbase dashboard (#131)

--- collector-dashboards/otel-collector-couchbasereceiver-dashboard/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "dashboard_url" {
-  value       = "https://app.lightstep.com/${var.lightstep_project}/dashboard/${lightstep_dashboard.otel_collector_couchbasereceiver_dashboard.id}"
-  description = "OpenTelemetry Couchbase Integration Dashboard URL"
-}
